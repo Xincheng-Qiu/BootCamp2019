85a5dbc82a3d9b80b6a92480002b1eea47809fd5
Xincheng-Qiu <44378668+Xincheng-Qiu@users.noreply.github.com>
2019-07-06 09:55:01 -0500

    change path

---
+++ ProblemSets/Comp1bPythonIntro/calculator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""This script is for Problem 3 in StandardLibrary
+   which contains:
+   a function that returns the sum of two arguments
+   a function that returns the product of two arguments
+"""
+
+def sum(x,y):
+    return x + y
+
+
+def product(x,y):
+    return x * y
